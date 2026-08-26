3f3fe70c0d6fb06e1799f6731a89631f2878588c
Tharun Adith <tharunvta@gmail.com>
2026-08-26 09:52:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0393-utf-8-validation/0393-utf-8-validation.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean validUtf8(int[] a) {
+        int rem=0;
+        for(int i=0;i<a.length;i++)
+        {
+            if(rem>0)
+            {
+                if((a[i]&0b11000000)!=0b10000000)
+                {
+                    return false;
+                }
+                rem--;
+              
+                
+            }
+            else
+            {
+             if((a[i]&0b10000000)==0b00000000)
+                {
+                    rem=0;
+                }
+                else   if((a[i]&0b11100000)==0b11000000)
+                {
+                    rem=1;
+                }
+                  else if((a[i]&0b11110000)==0b11100000)
+                {
+                    rem=2;
+                }
+                   else if((a[i]&0b11111000)==0b11100000)
+                {
+                    rem=3;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return rem==0;
+    }
+}
