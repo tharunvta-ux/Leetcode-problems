86caad09bd544f76c9caabbf96d1fd45dfd2235a
Tharun Adith <tharunvta@gmail.com>
2026-09-18 10:00:17 +0530

    Time: 1 ms (39.36%), Space: 46.5 MB (94.77%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -2,54 +2,55 @@ class Solution {
     public int r;
     public int c;
     public int [][]visit;
-    class pair
-    {
-       public int row;
-        public int col;
-        public pair(int row,int col)
-        {
-            this.row=row;
-            this.col=col;
-        }
-    }
+    public int ocolor;
+    // class pair
+    // {
+    //    public int row;
+    //     public int col;
+    //     public pair(int row,int col)
+    //     {
+    //         this.row=row;
+    //         this.col=col;
+    //     }
+    // }
     public int[][] floodFill(int[][] a, int sr, int sc, int color) {
         
         r=a.length;
         c=a[0].length;
         visit=new int[r][c];
-        int ocolor=a[sr][sc];
+         ocolor=a[sr][sc];
         if(ocolor==color)
         {
             return a;
         }
+        dfs(sr,sc,color,a);
+ return a;
+    }
+        // Queue<pair>q=new LinkedList<>();
 
-        Queue<pair>q=new LinkedList<>();
-        q.add(new pair(sr,sc));
+        // q.add(new pair(sr,sc));
+        public void dfs(int sr,int sc,int color,int [][]a)
+        {
         visit[sr][sc]=1;
         a[sr][sc]=color;
         int []dx={+1,0,-1,0};
         int []dy={0,+1,0,-1};
-        while(!q.isEmpty())
-        {
-            pair current=q.poll();
-            int nr=current.row;
-            int nc=current.col;
+        
             for(int i=0;i<4;i++)
             {
-                int nrow=nr+dx[i];
-                int ncol=nc+dy[i];
+                int nrow=sr+dx[i];
+                int ncol=sc+dy[i];
                 if (nrow >= 0 && nrow < r &&
                     ncol >= 0 && ncol < c &&
                     a[nrow][ncol] == ocolor &&
                     visit[nrow][ncol] == 0)
                     {
-                        q.add(new pair(nrow,ncol));
-                        visit[nrow][ncol]=1;
+                    
                         a[nrow][ncol]=color;
+                        dfs(nrow,ncol,color,a);
                     }
             }
+        
         }
-        return a;
 
-    }
 }
